b2a88ee664a7951f12ea8dba3542972fcf8c99c9
Kezie Todd <kezie.todd@Kezies-MacBook-Pro.local>
2019-12-05 00:12:37 +0000

    added drawing canvas

--- js/main.js
+++ js/main.js
@@ -29,3 +29,80 @@ $(function() {
        $(this).width($("#width_tmp_select").width());  
     });
    });
+
+
+   //CANVAS
+var can = document.getElementById("sheet");
+
+function resizeCanvas() {
+  can.style.width = window.innerWidth + "px";
+  setTimeout(function() {
+    can.style.height = window.innerHeight + "px";
+  }, 0);
+};
+
+// Webkit/Blink will fire this on load, but Gecko doesn't.
+window.onresize = resizeCanvas;
+
+// So we fire it manually...
+resizeCanvas();
+
+var canvas, stage;
+var drawingCanvas;
+var oldPt;
+var oldMidPt;
+var color;
+var stroke;
+var index;
+
+function init() 
+{
+  canvas = document.getElementById("sheet");
+  index = 0;
+
+  //check to see if we are running in a browser with touch support
+  stage = new createjs.Stage(canvas);
+  stage.autoClear = false;
+  stage.enableDOMEvents(true);
+
+  createjs.Touch.enable(stage);
+  createjs.Ticker.setFPS(24);
+
+  drawingCanvas = new createjs.Shape();
+
+  stage.addEventListener("stagemousedown", handleMouseDown);
+  stage.addEventListener("stagemouseup", handleMouseUp);
+
+  stage.addChild(drawingCanvas);
+  stage.update();
+}
+
+function stop() {}
+
+function handleMouseDown(event) {
+  color = "#85FF64";
+  stroke = 1;
+  oldPt = new createjs.Point(stage.mouseX, stage.mouseY);
+  oldMidPt = oldPt;
+  stage.addEventListener("stagemousemove" , handleMouseMove);
+}
+
+function handleMouseMove(event) {
+  var midPt = new createjs.Point(oldPt.x + stage.mouseX>>1, oldPt.y+stage.mouseY>>1);
+
+  drawingCanvas.graphics.clear().setStrokeStyle(stroke, 'round', 'round').beginStroke(color).moveTo(midPt.x, midPt.y).curveTo(oldPt.x, oldPt.y, oldMidPt.x, oldMidPt.y);
+
+  oldPt.x = stage.mouseX;
+  oldPt.y = stage.mouseY;
+
+  oldMidPt.x = midPt.x;
+  oldMidPt.y = midPt.y;
+
+  stage.update();
+}
+
+function handleMouseUp(event) {
+  stage.removeEventListener("stagemousemove" , handleMouseMove);
+}
+
+init();
